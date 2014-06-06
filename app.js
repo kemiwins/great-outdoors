525747b96793ed2905e14e39e6a62e7ef23168a8
kemiwins <kemi.mail@gmail.com>
2014-06-06 17:21:39 -0400

    loads once, error on second level array

--- app.js
+++ app.js
@@ -11,27 +11,22 @@ $(document).ready(function(){
 			"&q[state_eq]=" + stateValue + "&q[activities_activity_type_name_cont]=" + activityValue,
 			dataType: "jsonp",
 			type: "GET",
-			success: function(data){
+			success: function(data){	
 				console.log(data);
 
-				for(item in data["places"]) {
-					var locate = data["places"][item];
-						$(".info").append("<p>" + "Place: " + "<a href=" + locate["activities"][item]["url"] + " target=" + "_blank" + ">" + locate["name"] + "</a>" + "</p>");
+				for(var x in data.places) {
+				var locate = data.places[x];
+						$(".info").append("<p>" + "Place: " + locate["name"] + "</p>");
 						$(".info").append("<p>" + "Location: " + locate["city"] + ", " + locate["state"] + "</p>");
-						$(".info").append("<p>" + "Activities: " + locate["activities"][item]["activity_type_name"] + "</p>");
-						$(".info").append("<p>" + "Description: " + locate["activities"][item]["description"] + "</p>");
-						$(".image").append("<p>" + "<img src=" + locate["activities"][item]["thumbnail"] + ">" +"</p>");
+						$(".info").append("<p>" + "Activities: " + locate["activities"][x]["activity_type_name"] + "</p>");
+						$(".info").append("<p>" + "Description: " + locate["activities"][x]["description"] + "</p>");
+						$(".image").append("<p>" + "<img src=" + locate["activities"][x]["thumbnail"] + ">" +"</p>");
 				}
-
-				$("#city").val("");
+				/*$("#city").val("");
 				$("#state").val("Alabama");
-				$("#activity").val("camping");
-
+				$("#activity").val("camping");*/
 			}
+		
 		});
 	});
 });
-
-
-
-
