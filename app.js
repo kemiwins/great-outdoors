e28a93435a0e359a807f8b5ae12b1ef8cb567b66
kemiwins <kemi.mail@gmail.com>
2014-06-07 10:56:13 -0400

    second level array now working

--- app.js
+++ app.js
@@ -8,25 +8,24 @@ $(document).ready(function(){
 
 		$.ajax({
 			url: "https://outdoor-data-api.herokuapp.com/api.json?api_key=4016165acc967a9800153c77a3528d83&q[city_eq]=" + cityValue +
-			"&q[state_eq]=" + stateValue + "&q[activities_activity_type_name_cont]=" + activityValue,
+			"&q[state_eq]=" + stateValue + "&q[activities_activity_type_name_eq]=" + activityValue,
 			dataType: "jsonp",
 			type: "GET",
 			success: function(data){	
 				console.log(data);
 
-				for(var x in data.places) {
-				var locate = data.places[x];
+				for(var x in data["places"]) {	
+					var locate = data["places"][x];
 						$(".info").append("<p>" + "Place: " + locate["name"] + "</p>");
 						$(".info").append("<p>" + "Location: " + locate["city"] + ", " + locate["state"] + "</p>");
-						$(".info").append("<p>" + "Activities: " + locate["activities"][x]["activity_type_name"] + "</p>");
-						$(".info").append("<p>" + "Description: " + locate["activities"][x]["description"] + "</p>");
-						$(".image").append("<p>" + "<img src=" + locate["activities"][x]["thumbnail"] + ">" +"</p>");
+						$(".info").append("<p>" + "Activities: " + data["places"][x]["activities"][0]["activity_type_name"] + "</p>");
+						$(".info").append("<p>" + "Activities: " + data["places"][x]["activities"][0]["description"] + "</p>");
+						$(".image").append("<p>" + "<img src=" + data["places"][x]["activities"][0]["thumbnail"] + ">" +"</p>");	
 				}
 				/*$("#city").val("");
 				$("#state").val("Alabama");
 				$("#activity").val("camping");*/
-			}
-		
+			}		
 		});
 	});
 });
