6b02c8e894c45fe28f44c7173718ff355151b230
kemiwins <kemi.mail@gmail.com>
2014-06-07 16:16:59 -0400

    added no results msg and jquery to clear results

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 	
 	$(".button").click(function(){
+		$(".info").html(" ");
+		search();
 
-		var cityValue = $("#city").val();
-		var stateValue = $("#state option:selected").val();
-		var activityValue = $("#activity option:selected").val();
+	});
+});
 
+var search = function(){
+
+	var cityValue = $("#city").val();
+	var stateValue = $("#state option:selected").val();
+	var activityValue = $("#activity option:selected").val();
+	
 		$.ajax({
-			url: "https://outdoor-data-api.herokuapp.com/api.json?api_key=4016165acc967a9800153c77a3528d83&q[city_eq]=" + cityValue +
+			url: "https://outdoor-data-api.herokuapp.com/api.json?api_key=4016165acc967a9800153c77a3528d83&q[city_cont]=" + cityValue +
 			"&q[state_eq]=" + stateValue + "&q[activities_activity_type_name_eq]=" + activityValue,
 			dataType: "jsonp",
 			type: "GET",
@@ -16,16 +23,21 @@ $(document).ready(function(){
 
 				for(var x in data["places"]) {	
 					var locate = data["places"][x];
+						$(".info").append("<hr>");
 						$(".info").append("<p>" + "Place: " + locate["name"] + "</p>");
 						$(".info").append("<p>" + "Location: " + locate["city"] + ", " + locate["state"] + "</p>");
-						$(".info").append("<p>" + "Activities: " + data["places"][x]["activities"][0]["activity_type_name"] + "</p>");
-						$(".info").append("<p>" + "Activities: " + data["places"][x]["activities"][0]["description"] + "</p>");
-						$(".image").append("<p>" + "<img src=" + data["places"][x]["activities"][0]["thumbnail"] + ">" +"</p>");	
+						$(".info").append("<p>" + "Activity: " + "<a href=" + locate["activities"][0]["url"] + " target=_blank" + ">" + locate["activities"][0]["activity_type_name"] + "</a>" + "</p>");
+						$(".info").append("<p>" + "Description: " + locate["activities"][0]["description"] + "</p>");
+						/*$(".info").append("<p>" + "<img src=" + locate["activities"][0]["thumbnail"] + ">" +"</p>");	
+				
+					if ((locate["activities"][0]["thumbnail"]).val() === 0){
+									$(".image").append("<p>" + "" + "</p>");*/
 				}
-				/*$("#city").val("");
-				$("#state").val("Alabama");
-				$("#activity").val("camping");*/
+				if (data["places"].length === 0) {
+					console.log("no results");
+					$(".info").append("Sorry, no results!");
+				}
+				
 			}		
 		});
-	});
-});
+}
