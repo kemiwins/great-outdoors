16147881361395842d47379a7e34c0c25d01f53d
kemiwins <kemi.mail@gmail.com>
2014-06-08 12:22:28 -0400

    added hover colors, removed thumbnails

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 $(document).ready(function(){
-	
-	$(".info").error(function(){
-		alert("error");
-	})
-	$(".info div").text( "no pic");
-
+//clear any previous search results and load new search results//
 	$(".button").click(function(){
 		$(".info").html(" ");
 		$(".image").html(" ");
 		search();
-	});
-
-	
+	});	
 });
 
-var search = function(){
+	var search = function(){
+//get search values from input and dropdown//
+		var cityValue = $("#city").val();
+		var stateValue = $("#state option:selected").val();
+		var activityValue = $("#activity option:selected").val();
 
-	var cityValue = $("#city").val();
-	var stateValue = $("#state option:selected").val();
-	var activityValue = $("#activity option:selected").val();
-	
+//get api from TrailAPI.com and append array properties to html//	
 		$.ajax({
-			url: "https://outdoor-data-api.herokuapp.com/api.json?api_key=4016165acc967a9800153c77a3528d83&q[city_cont]=" + cityValue +
+			url: "https://outdoor-data-api.herokuapp.com/api.json?api_key=1add6ce6b3fb7c72550bc3b5060a7565&q[city_cont]=" + cityValue +
 			"&q[state_eq]=" + stateValue + "&q[activities_activity_type_name_eq]=" + activityValue,
 			dataType: "jsonp",
 			type: "GET",
@@ -31,23 +25,21 @@ var search = function(){
 				for(var x in data["places"]) {	
 					var locate = data["places"][x];
 						$(".info").append("<hr>");
-						$(".info").append("<div>" + "<img src=" + locate["activities"][0]["thumbnail"] + ">" +"</div>");
-						$(".info").append("<p>" + "Place: " + locate["name"] + "</p>");
+						/*$(".info").append("<div>" + "<img src=" + locate["activities"][0]["thumbnail"] + ">" +"</div>");*/
+						$(".info").append("<p>" + "Place: " + "<a href=" + locate["activities"][0]["url"] + " target=_blank" + ">" + locate["name"] + "</a>" + "</p>");
 						$(".info").append("<p>" + "Location: " + locate["city"] + ", " + locate["state"] + "</p>");
-						$(".info").append("<p>" + "Activity: " + "<a href=" + locate["activities"][0]["url"] + " target=_blank" + ">" + locate["activities"][0]["activity_type_name"] + "</a>" + "</p>");
+						$(".info").append("<p>" + "Activity: " +  locate["activities"][0]["activity_type_name"] + "</p>");
 						$(".info").append("<p>" + "Description: " + locate["activities"][0]["description"] + "</p>");						
 				}
-				/*var pic = (locate["activities"][0]["thumbnail"]);
-				if ( pic === null ) {
-					$(".info div").text( "no pic");
-				}*/
-					
 				if (data["places"].length === 0) {
 					console.log("no results");
-					$(".info").append("Sorry, no results! Try a different location or activity.");
+					$(".info").append("Sorry, no results! Please try a new activity or location.");
 				}	
-
-							
+				/*var pic = (locate["activities"][0]["thumbnail"]);
+				if ( pic === null ) {
+					$(".info div").text( "no pic available");
+				}*/
+											
 			}		
 		});
 }
